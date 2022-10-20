28cec47d8d57fe608bccb997a9f622a006a99acc
C-Macargo <caio.camargos97@gmail.com>
2022-10-20 11:55:40 -0300

    Projeto ParrotsCardGame embaralhando cartas sem layout

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
+function comparador() { 
+	return Math.random() - 0.5; 
+}
 
 let NumberOfCards
 const ContainerDiv = document.querySelector(".game-container")
-
+const CardFronts= ['bobrossparrot.gif','explodyparrot.gif','fiestaparrot.gif','metalparrot.gif','unicornparrot.gif','tripletsparrot.gif','revertitparrot.gif']
+let CardList =[]
 
 
 function SelectNumberOfCards() {
-    CardList =[]
-
 
 
     let NumberOfCards = Number(prompt("Com quantas cartas iremos jogar"))
 
 
-    if (NumberOfCards < 4 || NumberOfCards > 12  || NumberOfCards % 2 !== 0){
+    if (NumberOfCards < 4 || NumberOfCards > 14  || NumberOfCards % 2 !== 0){
         return SelectNumberOfCards()
     }
     else{
         i = 0
-            while (i  < NumberOfCards){
-                ContainerDiv.innerHTML +=  `<div class="card"></div>`
+            while (i  < NumberOfCards/2){
+                CardList.push(CardFronts[i]);
+                CardList.push(CardFronts[i]);
                 i++
         }
     }
+    SummonCards()
 }
+
+
+function SummonCards(){
+    CardList.sort(comparador)
+    for (let i = 0; i < CardList.length; i++) {
+        ContainerDiv.innerHTML +=  
+    `<div class="card" onclick="FlipCard(this)">
+    <div class="card-front"><img src="./assets/${CardList[i]}""></div>
+    <div class="card-back"><img src="./assets/card-back.png"></div>
+    </div>`
+    } 
+}
+
+function FlipCard(){
+
+}
+
